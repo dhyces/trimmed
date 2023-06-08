1b5a55775623c9731ec3adb2cd87769439d7eae1
dhyces <pokmon987@gmail.com>
2023-06-07 21:56:16 -0400

    Fix toast for 1.20

--- Common/src/main/java/dhyces/trimmed/impl/client/InfoToast.java
+++ Common/src/main/java/dhyces/trimmed/impl/client/InfoToast.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.toasts.Toast;
 import net.minecraft.client.gui.components.toasts.ToastComponent;
 import net.minecraft.network.chat.Component;
@@ -29,33 +30,33 @@ public class InfoToast implements Toast {
         return new InfoToast(Component.translatable("trimmed.info.datapacksReloadedTitle").withStyle(Style.EMPTY.withUnderlined(true)), Component.translatable("trimmed.info.datapacksReloaded"));
     }
 
+
     @Override
-    public Visibility render(PoseStack pPoseStack, ToastComponent pToastComponent, long pTimeSinceLastVisible) {
-        RenderSystem.setShaderTexture(0, TEXTURE);
+    public Visibility render(GuiGraphics guiGraphics, ToastComponent toastComponent, long timeSinceLast) {
         if (height() <= 32) {
-            Gui.blit(pPoseStack, 0, 0, 0, 0, width(), height());
+            guiGraphics.blit(TEXTURE, 0, 0, 0, 0, width(), height());
         } else {
-            Gui.blit(pPoseStack, 0, 0, 0, 0, width(), 4);
+            guiGraphics.blit(TEXTURE, 0, 0, 0, 0, width(), 4);
             int middleAmount = height()-8;
             int yPos = 4;
             int iterTimes = (int) Math.ceil(middleAmount / 28f);
             for (; iterTimes >= 0; iterTimes--) {
                 int amountToDraw = Math.min(24, middleAmount);
-                Gui.blit(pPoseStack, 0, yPos, 0, 4, width(), amountToDraw);
+                guiGraphics.blit(TEXTURE, 0, yPos, 0, 4, width(), amountToDraw);
                 middleAmount -= amountToDraw;
                 yPos += amountToDraw;
             }
-            Gui.blit(pPoseStack, 0, yPos, 0, 28, width(), 4);
+            guiGraphics.blit(TEXTURE, 0, yPos, 0, 28, width(), 4);
         }
 
-        Font font = pToastComponent.getMinecraft().font;
-        Gui.drawString(pPoseStack, font, title, 5, 5, 0xFFFFFF);
+        Font font = toastComponent.getMinecraft().font;
+        guiGraphics.drawString(font, title, 5, 5, 0xFFFFFF);
         int lineNum = 1;
         for (FormattedCharSequence messageLine : messageLines) {
-            Gui.drawString(pPoseStack, font, messageLine, 5, 7 + ((font.lineHeight+2) * lineNum++), 0xFFFFFF);
+            guiGraphics.drawString(font, messageLine, 5, 7 + ((font.lineHeight+2) * lineNum++), 0xFFFFFF);
         }
 
-        return pTimeSinceLastVisible > 5000f * pToastComponent.getNotificationDisplayTimeMultiplier() ? Visibility.HIDE : Visibility.SHOW;
+        return timeSinceLast > 5000f * toastComponent.getNotificationDisplayTimeMultiplier() ? Visibility.HIDE : Visibility.SHOW;
     }
 
     @Override
