772f4c3ffad776a7757cb7a5e8448957d57a6fc0
dhyces <pokmon987@gmail.com>
2023-06-12 14:51:41 -0400

    Fix networking

--- Common/src/main/java/dhyces/trimmed/impl/network/Networking.java
+++ Common/src/main/java/dhyces/trimmed/impl/network/Networking.java
@@ -1,21 +1,24 @@
 package dhyces.trimmed.impl.network;
 
 import commonnetwork.api.Network;
+import commonnetwork.networking.data.PacketContext;
 import dhyces.modhelper.network.handler.PacketHandler;
 import dhyces.modhelper.network.packet.CommonPacket;
 import dhyces.trimmed.Trimmed;
 import dhyces.trimmed.TrimmedClient;
 import dhyces.trimmed.impl.network.packets.SyncMapsPacket;
 
+import java.util.function.Consumer;
+
 public class Networking {
 
     public static final CommonPacket.Type<SyncMapsPacket> SYNC_MAPS = new CommonPacket.Type<>(SyncMapsPacket.class, Trimmed.id("sync_maps"), SyncMapsPacket::new);
 
     public static void init() {
-        registerPacket(SYNC_MAPS, TrimmedClient.CLIENT_HANDLER);
+        registerPacket(SYNC_MAPS, packet -> TrimmedClient.CLIENT_HANDLER.getPacketConsumer(SYNC_MAPS).handle(packet));
     }
 
-    private static <T extends CommonPacket<T>> void registerPacket(CommonPacket.Type<T> type, PacketHandler packetHandler) {
-        Network.registerPacket(type.id(), type.clazzType(), CommonPacket::writeTo, type.factory(), packetHandler.getPacketConsumer(type)::handle);
+    private static <T extends CommonPacket<T>> void registerPacket(CommonPacket.Type<T> type, Consumer<PacketContext<T>> packetHandler) {
+        Network.registerPacket(type.id(), type.clazzType(), CommonPacket::writeTo, type.factory(), packetHandler);
     }
 }
