a255198e0e2d688e0b1be399462e05620a51a6fa
dhyces <10985914+dhyces@users.noreply.github.com>
2024-05-24 23:51:51 -0700

    Update registered id

--- Common/src/main/java/dev/dhyces/trimmed/TrimmedClient.java
+++ Common/src/main/java/dev/dhyces/trimmed/TrimmedClient.java
@@ -30,7 +30,7 @@ public class TrimmedClient {
         TrimmedSpriteSourceTypes.bootstrap();
 //        ModelTemplateManager.init();
         ItemOverrideProviderRegistry.init();
-        MapKeyResolvers.register(Trimmed.id("trim_texture"), ClientMapKeyResolvers.TEXTURE);
+        MapKeyResolvers.register(Trimmed.id("texture"), ClientMapKeyResolvers.TEXTURE);
         ClientMapManager.registerBaseKey(ClientMapKeys.TRIM_MATERIALS);
     }
 
