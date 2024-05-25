134be4d063ed7e158845dadd7c693ddb07c753b5
dhyces <10985914+dhyces@users.noreply.github.com>
2024-05-24 21:59:28 -0700

    Rename to a better name

--- Common/src/main/java/dev/dhyces/trimmed/api/client/ClientMapKeys.java
+++ Common/src/main/java/dev/dhyces/trimmed/api/client/ClientMapKeys.java
@@ -5,6 +5,6 @@ import dev.dhyces.trimmed.impl.client.maps.MapKey;
 import net.minecraft.resources.ResourceLocation;
 
 public class ClientMapKeys {
-    public static final MapKey<ResourceLocation, ResourceLocation> TRIM_MATERIALS = MapKey.of(ClientMapTypes.TRIM_MATERIALS, Trimmed.id("material_suffixes"));
+    public static final MapKey<ResourceLocation, ResourceLocation> TRIM_MATERIALS = MapKey.of(ClientMapTypes.MATERIAL_SUFFIXES, Trimmed.id("material_suffixes"));
     public static final MapKey<ResourceLocation, ResourceLocation> DARKER_TRIM_MATERIALS = TRIM_MATERIALS.makeSubKey("darker_material_suffixes");
 }
--- Common/src/main/java/dev/dhyces/trimmed/impl/client/atlas/OpenPalettedPermutations.java
+++ Common/src/main/java/dev/dhyces/trimmed/impl/client/atlas/OpenPalettedPermutations.java
@@ -24,7 +24,6 @@ import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.IntUnaryOperator;
@@ -34,7 +33,7 @@ public class OpenPalettedPermutations implements SpriteSource {
     public static final MapCodec<OpenPalettedPermutations> CODEC = RecordCodecBuilder.mapCodec(instance ->
             instance.group(
                     ResourceLocation.CODEC.fieldOf("palette_key").forGetter(openPalettedPermutations -> openPalettedPermutations.paletteKey),
-                    MapKey.codec(ClientMapTypes.TRIM_MATERIALS).fieldOf("permutation_map").forGetter(openPalettedPermutations -> openPalettedPermutations.permutations.getKey()),
+                    MapKey.codec(ClientMapTypes.MATERIAL_SUFFIXES).fieldOf("permutation_map").forGetter(openPalettedPermutations -> openPalettedPermutations.permutations.getKey()),
                     ClientTagKey.CODEC.fieldOf("texture_set").forGetter(openPalettedPermutations -> openPalettedPermutations.textures)
             ).apply(instance, OpenPalettedPermutations::new)
     );
