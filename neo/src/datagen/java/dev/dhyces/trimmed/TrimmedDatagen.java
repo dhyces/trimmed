ef8f13beebfb6776a408766cec78fc30c1cacad8
dhyces <10985914+dhyces@users.noreply.github.com>
2024-05-28 19:55:30 -0700

    Rewrite datagen entrypoint as an actual entrypoint

--- neo/src/datagen/java/dev/dhyces/trimmed/TrimmedDatagen.java
+++ neo/src/datagen/java/dev/dhyces/trimmed/TrimmedDatagen.java
@@ -3,16 +3,17 @@ package dev.dhyces.trimmed;
 import dev.dhyces.trimmed.model.VanillaItemModelOverrideProvider;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
-// as much as I dislike EBS magic annotations, it is the only way I see of actually separating datagen from main src
-@EventBusSubscriber(modid = Trimmed.MODID, bus = EventBusSubscriber.Bus.MOD)
+@Mod(Trimmed.MODID)
 public class TrimmedDatagen {
+    public TrimmedDatagen(IEventBus modBus) {
+        modBus.addListener(this::setupDatagen);
+    }
 
-    @SubscribeEvent
-    static void setupDatagen(final GatherDataEvent event) {
+    private void setupDatagen(final GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
 
