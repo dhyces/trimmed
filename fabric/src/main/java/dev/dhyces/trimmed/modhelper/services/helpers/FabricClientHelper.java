519b413ed05660f2603e9743976fcbdfd88d5204
dhyces <10985914+dhyces@users.noreply.github.com>
2024-05-28 21:06:41 -0700

    Remove old code

--- fabric/src/main/java/dev/dhyces/trimmed/modhelper/services/helpers/FabricClientHelper.java
+++ fabric/src/main/java/dev/dhyces/trimmed/modhelper/services/helpers/FabricClientHelper.java
@@ -1,17 +1,8 @@
 package dev.dhyces.trimmed.modhelper.services.helpers;
 
-import com.mojang.blaze3d.platform.NativeImage;
 import dev.dhyces.trimmed.api.client.TrimmedClientApiEntrypoint;
 import dev.dhyces.trimmed.impl.ModApiConsumer;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.texture.SpriteContents;
-import net.minecraft.client.resources.metadata.animation.FrameSize;
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceMetadata;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 
 import java.util.List;
 
@@ -20,15 +11,5 @@ public class FabricClientHelper implements ClientHelper {
     public List<ModApiConsumer<TrimmedClientApiEntrypoint>> getClientApiConsumers() {
         return FabricLoader.getInstance().getEntrypointContainers("trimmed:client_api_entrypoint", TrimmedClientApiEntrypoint.class)
                 .stream().map(container -> new ModApiConsumer<>(container.getProvider().getMetadata().getId(), container.getEntrypoint())).toList();
-    }
-
-    @Override
-    public SpriteContents createSpriteContents(ResourceLocation id, NativeImage image) {
-        return new SpriteContents(id, new FrameSize(image.getWidth(), image.getHeight()), image, ResourceMetadata.EMPTY);
-    }
-
-    @Override
-    public ModelResourceLocation getModelLocation(ItemStack itemStack) {
-        return Minecraft.getInstance().getItemRenderer().getItemModelShaper().shapes.get(Item.getId(itemStack.getItem()));
     }
 }
--- neo/src/main/java/dev/dhyces/trimmed/modhelper/services/helpers/NeoClientHelper.java
+++ neo/src/main/java/dev/dhyces/trimmed/modhelper/services/helpers/NeoClientHelper.java
@@ -1,18 +1,9 @@
 package dev.dhyces.trimmed.modhelper.services.helpers;
 
-import com.mojang.blaze3d.platform.NativeImage;
 import dev.dhyces.trimmed.api.TrimmedClientApi;
 import dev.dhyces.trimmed.api.client.TrimmedClientApiEntrypoint;
 import dev.dhyces.trimmed.impl.ModApiConsumer;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.texture.SpriteContents;
-import net.minecraft.client.resources.metadata.animation.FrameSize;
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceMetadata;
-import net.minecraft.world.item.ItemStack;
 import net.neoforged.fml.ModList;
-import net.neoforged.neoforge.client.model.RegistryAwareItemModelShaper;
 
 import java.lang.annotation.ElementType;
 import java.lang.reflect.InvocationTargetException;
@@ -42,15 +33,5 @@ public class NeoClientHelper implements ClientHelper {
                     }
                     return new ModApiConsumer<>(modid, apiEntrypoint);
                 }).toList();
-    }
-
-    @Override
-    public SpriteContents createSpriteContents(ResourceLocation id, NativeImage image) {
-        return new SpriteContents(id, new FrameSize(image.getWidth(), image.getHeight()), image, ResourceMetadata.EMPTY);
-    }
-
-    @Override
-    public ModelResourceLocation getModelLocation(ItemStack itemStack) {
-        return ((RegistryAwareItemModelShaper) Minecraft.getInstance().getItemRenderer().getItemModelShaper()).getLocation(itemStack);
     }
 }
