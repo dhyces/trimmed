b2b493c33cf704ea85c06d4691bf3d7030514e2c
dhyces <10985914+dhyces@users.noreply.github.com>
2024-06-16 19:40:13 -0700

    Update test mod

--- testmods/neo/build.gradle.kts
+++ testmods/neo/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
 		parchment("org.parchmentmc.data:parchment-${libs.versions.parchment.mc.get()}:${libs.versions.parchment.release.get()}@zip")
 	})
 
-	implementation("dev.dhyces.trimmed:trimmed-neo-1.20.6:2.1.4")
+	implementation("dev.dhyces.trimmed:trimmed-neo-1.21:3.0.0")
 //	compileOnly(project(":common"))
 //	compileOnly(project(":neo"))
 //	neoJava(project(path = ":neo", configuration = "neoJava"))
